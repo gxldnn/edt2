df14ead7d118f1fc50974cf3751f7d9a02c74d53
gxldnn <riberajan@gmail.com>
2025-09-23 19:35:50 +0000

    asd

--- Administracio de Sistemes/Ac1.2/01.multiplicar1.sh
+++ Administracio de Sistemes/Ac1.2/01.multiplicar1.sh
@@ -4,22 +4,21 @@ read -p "Digues un numero per a mostrar la seva taula de multiplicar : " num
 
 nums=(0 1 2 3 4 5 6 7 8 9)
 
-if  [ $num = 0 ] || [ -z "$num" || ! "$num" =~ ^[0-9]+$ ]]; then
-
-    echo "El numero es zero o invalid, la seva taula es = 0 tot jejejej"
+if ! [[ "$num" =~ ^[0-9]+$ ]] || [ "$num" -le 0 ]; then ## mirar si es text 0 si es major que 0
+    echo "Numero invalid o zero."
     exit 0
 fi
 
 
-nums=$(($num + 1))
-numi=$(($num - 1))
+nummas=$(($num + 1))
+nummenos=$(($num - 1))
 
 for i in ${nums[@]}; do
-    echo -e "$nums * $i = $(($i * $nums))" 
+    echo -e "$nummas * $i = $(($i * $nummas))" 
 done
 
 echo ""
 
 for i in ${nums[@]}; do
-    echo -e "$numi * $i = $(($i * $numi))" 
+    echo -e "$nummenos * $i = $(($i * $nummenos))" 
 done
