61ce1cdea292caa084daeba9a109a3830e6e8610
gxldnn <riberajan@gmail.com>
2025-09-25 17:34:34 +0000

    asd

---
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+
+## FLAGS, marquem unes variables com a flags per a no fer cadenes de ifs infinites
+
+flagtype=""
+
+read -p "Introdueix un fitxer o directori per a o veure (tambe si vols, la seva ruta): " input
+if [ -f "$input" ]; then 
+    flagtype="arxiu"
+    action="llegir"
+elif [ -d "$input" ]; then
+    flagtype="directori"
+    action="veure"
+else
+    echo "El $flagtype o directori no existeix!!"
+    exit 1
+fi
+
+function compactar(){
+    
+    read -p "Vols $action el contingut del $flagtype? (s/n): " readfile
+    if [ "$readfile" = "s" ]; then
+        echo "Contingut del $flagtype $input:"
+        if [ $action = "veure" ];then
+            ls -laF $input
+        else
+            cat "$input"
+        fi
+    else
+       echo "No es $action el contingut de $flagtype" 
+    fi
+    echo ""
+    read -p "Vols comprimir el $flagtype? (s/n): " filecomp
+    if [ "$filecomp" = "s" ]; then
+        read -p "On vols guardar el $flagtype comprimit (Ruta completa)? Deixa-ho vuit per a desar al directori actual: " pathcomp
+        if [ -z $pathcomp ]; then
+            pathcomp="."
+            tar -czf "comprimit.tar.gz" "$input"
+            echo "$flagtype comprimit creat a: $(pwd))\comprimit.tar.gz"
+        else
+            tar -czf $pathcomp$input.tar.gz "$input"
+            echo "$flagtype comprimit creat a: $pathcomp/$input.tar.gz"
+        fi
+    else
+        echo "No es comprimira el $flagtype"
+    fi
+}
+
+function descompacta(){
+    
+
+}
+
+
+compactar
+read -p "Presiona [ENTER] per a continuar...."
+exit 0
