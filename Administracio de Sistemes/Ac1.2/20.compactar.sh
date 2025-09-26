ef5b7693f647f4ac0d0056b38d3b734c7fd464af
gxldnn <riberajan@gmail.com>
2025-09-26 18:35:05 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -47,6 +47,7 @@ function descompacta(){
     fi
 }
 
+read -p "Introdueix un fitxer o directori o tar.gz (tambe si vols, la seva ruta): " input
 
 if [[ "$input" == *.tar.gz ]]; then
     descompacta "$input"
