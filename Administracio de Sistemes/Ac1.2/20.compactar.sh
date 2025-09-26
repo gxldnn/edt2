9022a781c801a5cca7bc063cae12fe591c5baddb
gxldnn <riberajan@gmail.com>
2025-09-26 18:20:11 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -39,7 +39,7 @@ function descompacta(){
         if [ -z $pathdecomp ]; then 
             tar -zxf $input 
         else
-        tar -zxf $input -C 
+        tar -zxf $input -C $pathdecomp
 
 
     
