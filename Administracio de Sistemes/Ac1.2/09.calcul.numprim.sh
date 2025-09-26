8b0a3dd34a087679d210aac97dc865cc1a15d73f
gxldnn <riberajan@gmail.com>
2025-09-26 20:06:56 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/09.calcul.numprim.sh
+++ Administracio de Sistemes/Ac1.2/09.calcul.numprim.sh
@@ -7,7 +7,7 @@ count=0
 
 
 
-for((i=$num; i!=0; i--)); do
+for((i=$num; i!0; i--)); do
     if [ $(($num  % $i)) -eq 0 ]; then # Si la divisio no es exaxcta "-eq 0" no es compta
         (( count++ ))
     fi
