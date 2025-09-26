5e4dcc39ea81e59cf92c446c93c10e74044a1bd5
gxldnn <riberajan@gmail.com>
2025-09-26 18:17:53 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -35,7 +35,7 @@ function compactar(){
 function descompacta(){
     read  -p "Digues el nom d'un arxiu tar.gz o el seu path sencer: " $input
     if [ -e $input ]; then
-        read "Vols descomp"
+        read "Vols descomprimir el ar"
     tar -zxf $input
 
 
