1ca80198ef9f999e26492c52a7379e2eb2e1fab7
gxldnn <riberajan@gmail.com>
2025-09-26 19:51:13 +0000

    ClassPush

---
+++ Administracio de Sistemes/Ac1.2/07.vocals.consonants.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+clear
+read -p “Introdueix una paraula: ” paraula
+read -p “Introdueix la longitud: ” longitud
+pila=0
+comptador=1
+while [ $comptador -le $longitud ]; do
+aux=$(echo $paraula | cut -c $comptador)
