7fd76056d3d3c5993f76fbe1a87d27c689f7a813
gxldnn <riberajan@gmail.com>
2025-09-29 19:46:01 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -20,7 +20,7 @@ main(){
     if [ $input = *.tar.gz ];then
         read -p "Vols descomprimir aquesta arxiu?" decomp
         if [ $decomp = "s" ];then
-            read -p "A on el vols, no escriguis res per a compactarlo aqui"
+            read -p "A on el vols, no escriguis res per a compactarlo aqui " pathdecomp
             tar -zxvf -C 
     if [ -f $input ];then
         read -p "Vols llegir el contingut del fitxer? (s/n): " llegir
