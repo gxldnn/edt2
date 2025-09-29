3b854b0ce9a64aac1c3a01034595655c3976d7e1
gxldnn <riberajan@gmail.com>
2025-09-29 19:37:55 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -51,8 +51,8 @@ main(){
     else
         read -p "L'arxiu o directori que dius no existeix o el sistema no el troba, que vols crear ara, un arxiu direcotri o res (a/d/res)" choice
         if [ $choice = "a" ]; then
-            read -p "Digues el nom del nou arxiu"
-            
+            read -p "Digues el nom del nou arxiu" namearxiu
+
             touch 
         elif [ $choice = "d" ];then
         else []
