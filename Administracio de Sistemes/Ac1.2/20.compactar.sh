d54f458e2991ba5c1b383f35b3e94ce4b9288170
gxldnn <riberajan@gmail.com>
2025-09-29 19:23:59 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 read -p "Introdueix un fitxer o directori o tar.gz (El seu nom EXACTE): " name
-if [[ $((which locate)) = false ]]; then
-    echo "El programa locate no esta instalat. Instal·la'l i torna-ho a provar"
-    exit 1
-fi
+
 
 locate $input | grep -v "Trash"
 
