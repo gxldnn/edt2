7636adaf4204130a0b557ad67ed2a6d3471c3a39
gxldnn <riberajan@gmail.com>
2025-09-29 18:39:37 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
 
+## FLAGS, marquem unes variables com a flags per a no fer cadenes de ifs infinites
 
+function compactar(){
+
+    read -p "Vols $action el contingut del $flagtype? (s/n): " readfile
+    if [ "$readfile" = "s" ]; then
+        echo "Contingut del $flagtype $1:"
+        if [ $action = "veure" ];then
+            ls -laF $1
+        else
+            cat "$1"
+        fi
+    else
+       echo "No es pot $action el contingut de $flagtype" 
+    fi
+    echo ""
+    read -p "Vols comprimir el $flagtype? (s/n): " filecomp
+    if [ "$filecomp" = "s" ]; then
+        read -p "On vols guardar el $flagtype comprimit (Ruta completa)? Deixa-ho vuit per a desar al directori actual: " pathcomp
+        if [ -z $pathcomp ]; then
+            pathcomp="."
+            tar -czf "comprimit.tar.gz" "$1"
+            echo "$flagtype comprimit creat a: $(pwd)\comprimit.tar.gz"
+        else
+            tar -czf $pathcomp$1.tar.gz "$1"
+            echo "$flagtype comprimit creat a: $pathcomp/$1.tar.gz"
+        fi
+    else
+        echo "No es comprimira el $flagtype"
+    fi
+}
+
+function descompacta(){
+    if [ -e $1 ]; then
+        read -p "A on vols descomprimir el arxiu, path completa o relativa, no escriguis res per a guardar aqui: " pathdecomp
+        if [ -z $pathdecomp ]; then 
+            tar -zxf $1 
+            echo "Fitxer descomprès correctament a $(pwd)/$1"
+        else
+            tar -zxf $1 -C $pathdecomp
+            echo "Fitxer descomprès correctament a $pathdecomp"
+
+        fi
+    fi
+}
