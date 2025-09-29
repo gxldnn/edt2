71410a417117ef9776ee50f9153bfaeb4833977d
gxldnn <riberajan@gmail.com>
2025-09-29 18:26:10 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/09.calcul.numprim.sh
+++ Administracio de Sistemes/Ac1.2/09.calcul.numprim.sh
@@ -3,7 +3,7 @@
 read -p "Indica un primer numero per al interval: " num1
 read -p "Indica un segon numero per al interval: " num2
 
-if [ $topmnum > 1000  || $num1]; then
+if [ $num2  > 1000  || $num1]; then
     echo "El numero maxim ha de ser menor que 1000"
     exit 1
 fi
