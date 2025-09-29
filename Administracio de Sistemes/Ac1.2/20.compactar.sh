21e3056e3c501eed20dff84da528940b149467eb
gxldnn <riberajan@gmail.com>
2025-09-29 19:47:17 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -78,7 +78,7 @@ main(){
             fi
 
         elif [ $choice = "d" ];then
-            read -p "Digues el nom del nou arxiu" namearxiu
+            read -p "Digues el nom del nou directoru" namearxiu
             touch $namearxiu
             read -p "El vols editar ara mateix?: (s/n)" modificar
             if [ $modificar = "s" ];then
