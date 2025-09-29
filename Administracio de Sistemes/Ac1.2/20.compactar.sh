4ccc05a0d380578a061daf4a1b5cf8f50d62fb57
gxldnn <riberajan@gmail.com>
2025-09-29 19:28:06 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -18,9 +18,9 @@ input=$(locate $name | grep -v "Trash")
 
 main(){
     if [ -f $input ];then
+        
 
 
-    
     elif [ -d $input ]; then
     else
         echo -e "El fitxer o directori no existeix"
