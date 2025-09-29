af55f7e1bf25e475664fb71605c5b137ddea9d65
gxldnn <riberajan@gmail.com>
2025-09-29 19:47:42 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -80,6 +80,7 @@ main(){
         elif [ $choice = "d" ];then
             read -p "Digues el nom del nou directoru" namedir
             mkdir -p namedir
+            
             read -p "El vols comprimir? (s/n)" compactarnew
             if [ $compactarnew = "s"]; then
                 read -p "A on el vols compactar? no escriguis res per a guardar en el directori actual: " pathnew
