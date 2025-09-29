7592653f5a5ce0e0698a31ebaaba797ef661fa04
gxldnn <riberajan@gmail.com>
2025-09-29 20:44:05 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -76,7 +76,7 @@ main(){
                     pathnew="$(pwd)"
                 fi
                 namearxiu="$(basename "$namearxiu")"
-                tar cvf "$pathnew/$namearxiu.tar" -C "$(dirname "$input")" "$namearixu"
+                tar cvf "$pathnew/$namearxiu.tar" -C "$(dirname "$input")" "$namearixu"  2&
             fi
         elif [ $choice = "d" ];then
             read -p "Digues el nom del nou directoru" namedir
