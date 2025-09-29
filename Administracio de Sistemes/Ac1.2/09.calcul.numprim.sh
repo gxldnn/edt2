4e257a41f69b363edc7485873f91a27026ee4d3e
gxldnn <riberajan@gmail.com>
2025-09-29 18:15:39 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/09.calcul.numprim.sh
+++ Administracio de Sistemes/Ac1.2/09.calcul.numprim.sh
@@ -15,9 +15,17 @@ if [ $num1 -gt $num2 ];then
 fi
 
 for((i=$lownum; i>$topnum; i++)); do
-        if [ $(($i % )) -eq 0 ]; then # Si la divisio no es exaxcta "-eq 0" no es compta
+    for((i=2; i<=$num; i++)); do
+    if [ $(($num  % $i)) -eq 0 ]; then # Si la divisio no es exaxcta "-eq 0" no es compta
         (( count++ ))
     fi
+done
+
+if [ $count = 1 ]; then
+    echo "El numero es prim"
+else
+    echo "El numero no es prim"
+fi
 done
 
 
