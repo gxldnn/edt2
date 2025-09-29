8893ad86ca4ad94c04c5009899fda0ae20090c95
gxldnn <riberajan@gmail.com>
2025-09-29 19:30:24 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -18,7 +18,7 @@ input=$(locate $name | grep -v "Trash")
 
 main(){
     if [ -f $input ];then
-        
+        read -p "Vols llegir el conting"
         read -p "Vols compactar aquest arxiu: $input? (s/n): " compactararxiu
         if [ $compactararxiu = "s"]
             read -p "A on el vols compactar?: " pathca
