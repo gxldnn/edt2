fca02432b48045f822e2c857ce46959ccc62dbff
gxldnn <riberajan@gmail.com>
2025-09-29 20:41:33 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -75,7 +75,7 @@ main(){
                 if [ -z "$pathcd" ]; then
                     pathcd="$(pwd)"
                 fi
-                namearxiu="$(basename "$namearxiu")"
+                namearxiu="$(basename "$input")"
                 tar cvf "$pathcd/$namearxiu.tar" -C "$(dirname "$input")" "$namearixu"
             fi
         elif [ $choice = "d" ];then
