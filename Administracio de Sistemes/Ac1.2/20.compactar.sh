8beb097e88537cf8adaa007b838c803eb0e8a781
gxldnn <riberajan@gmail.com>
2025-09-29 19:44:52 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -18,7 +18,7 @@ input=$(locate $name | grep -v "Trash")
 
 main(){
     if [ $input = *.tar.gz ];then
-    
+        read -p ""
     if [ -f $input ];then
         read -p "Vols llegir el contingut del fitxer? (s/n): " llegir
         if [ $llegir = "s"];then
