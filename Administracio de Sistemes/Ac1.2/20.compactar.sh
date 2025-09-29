28c7ff3adc4f188e55e2e10c2af5e0b3b5588b62
gxldnn <riberajan@gmail.com>
2025-09-29 20:27:01 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -76,7 +76,7 @@ main(){
                 if [ -z $pathnew ];then
                     tar cvf "$input" -C "$(pwd)"
                 else
-                    tar cvf $input -C $pathcd
+                    tar cvf $input -C "$pathcd
                 fi
             fi
 
