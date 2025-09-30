e523c02e3a7c3679abb128be61508573eaae0529
gxldnn <riberajan@gmail.com>
2025-09-30 20:14:35 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/23.ncertanumero.sh
+++ Administracio de Sistemes/Ac1.2/23.ncertanumero.sh
@@ -2,19 +2,14 @@
 
 #read -n 1 -p "Escull un numero entre 1 o 2: " num
 
-for ((i=0; i<10000; i++)); do
-    random=$(( $RANDOM % 11))
-    echo "$random"
-    
-done
 
 
 
 
 
 
-#if [ $num -eq $random ];then
-#    echo -e "\nHas encertat!"
-#else
-#    echo -e "\nQuina pena"
-#fi
+if [ $num -eq $random ];then
+    echo -e "\nHas encertat!"
+else
+    echo -e "\nQuina pena"
+fi
