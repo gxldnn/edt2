508bad32c755d668c87afada3439c26c2c2cab82
gxldnn <riberajan@gmail.com>
2025-09-30 20:03:41 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/22.caraocreu.sh
+++ Administracio de Sistemes/Ac1.2/22.caraocreu.sh
@@ -5,7 +5,7 @@ read -n 1 -p "Escull un numero entre 1 o 2: " num
 random=$(( $RANDOM % 2 + 1))
 
 if [ $num -eq $random ];then
-    echo -e "Has encertat!"
+    echo -e "\nHas encertat!"
 else
     echo -e "Quina pena"
 fi
