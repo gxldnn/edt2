b2506c75509c2bc9ed030bba7b135dfd95562a88
gxldnn <riberajan@gmail.com>
2025-10-03 19:40:26 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -24,7 +24,7 @@ main(){
     if [[ "$input" == *.tar.gz ]]; then # si es un tar.gz
         read -p "Vols descomprimir aquesta arxiu?" decomp
         if [ $decomp = "s" ];then # si vol descomprimir
-            read -p "A on el vols, no escriguis res per a compactarlo aqui " pathdecomp
+            read -p "A on el vols, no escriguis res per a compactarlo aqui " pathdecomp # demana on
             if [ $pathdecomp = "" ]; then
                 tar -zxvf -C $(pwd)
             else
