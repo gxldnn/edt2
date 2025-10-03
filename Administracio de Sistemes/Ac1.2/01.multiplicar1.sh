0889c06996829dc903018b9eeceb9b1b8bb4f492
gxldnn <riberajan@gmail.com>
2025-10-03 18:34:43 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/01.multiplicar1.sh
+++ Administracio de Sistemes/Ac1.2/01.multiplicar1.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+    #!/bin/bash
 
-read -p "Digues un numero per a mostrar la seva taula de multiplicar : " num
+    read -p "Digues un numero per a mostrar la seva taula de multiplicar : " num
 
-nums=(0 1 2 3 4 5 6 7 8 9)
+    nums=(0 1 2 3 4 5 6 7 8 9)
 
-if ! [[ "$num" =~ ^[0-9]+$ ]] || [ "$num" -le 0 ]; then ## mirar si es text 0 si es major que 0
-    echo "Numero invalid o zero."
-    exit 0
-fi
+    if ! [[ "$num" =~ ^[0-9]+$ ]] || [ "$num" -le 0 ]; then ## mirar si es text 0 si es major que 0
+        echo "Numero invalid o zero."
+        exit 0
+    fi
 
 
-for i in ${nums[@]}; do # per cada numero de la llista nums
-    echo -e "$num * $i = $(($i * $num))" # for que recorre amb un numero i multiplica
-done
+    for i in ${nums[@]}; do # per cada numero de la llista nums
+        echo -e "$num * $i = $(($i * $num))" # for que recorre amb un numero i multiplica
+    done
 
