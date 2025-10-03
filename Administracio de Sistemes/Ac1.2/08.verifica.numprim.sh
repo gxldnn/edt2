f87647cb0793d074118d92ca5e6202d8f8e9f98f
gxldnn <riberajan@gmail.com>
2025-10-03 18:38:19 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/08.verifica.numprim.sh
+++ Administracio de Sistemes/Ac1.2/08.verifica.numprim.sh
@@ -3,7 +3,7 @@
 read -p "Indica un numero per a comprovar si es prim o no: " num
 count=0
 for((i=2; i<=$num; i++)); do # For que va des del 2 fins al numero introduit
-    if [ $(($num  % $i)) -eq 0 ]; then # Si la divisio + es exaxcta "-eq 0" +
+    if [ $(($num  % $i)) -eq 0 ]; then # Si la divisio  es exaxcta "-eq 0" +
         (( count++ ))
     fi
 done
