d0c8700580a82e1855997fe8156a94cd90df5c4c
gxldnn <riberajan@gmail.com>
2025-10-03 18:30:29 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/02.taula.multiplicar2.sh
+++ Administracio de Sistemes/Ac1.2/02.taula.multiplicar2.sh
@@ -2,7 +2,7 @@
 
 read -p "Digues un numero per a mostrar la seva taula de multiplicar : " num
 
-nums=(0 1 2 3 4 5 6 7 8 9)
+nums=(0 1 2 3 4 5 6 7 8 9) #
 
 if ! [[ "$num" =~ ^[0-9]+$ ]] || [ "$num" -le 0 ]; then ## mirar si es text 0 si es major que 0
     echo "Numero invalid o zero."
