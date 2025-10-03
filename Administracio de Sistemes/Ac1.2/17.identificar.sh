99142b8245036f6c25d60dfbb2b337dbd324dfb0
gxldnn <riberajan@gmail.com>
2025-10-03 19:27:48 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/17.identificar.sh
+++ Administracio de Sistemes/Ac1.2/17.identificar.sh
@@ -7,10 +7,10 @@ read -n1 tecla
 
 # Comprovar si es lletra
 if [[ $tecla =~ [a-zA-Z] ]]; then
-    echo -e "\nLa tecla \"$tecla es una lletra."
+    echo -e "\nLa tecla \"$tecla\" es una lletra."
 # Comprovar si es numero
 elif [[ $tecla =~ [0-9] ]]; then
-    echo -e "\nLa tecla $tecla es un numero."
+    echo -e "\nLa tecla $tecla\" es un numero."
 # Si no es cap de les anteriors, es caracter especial
 else
     echo -e "\nLa tecla $tecla es un caracter especial."
