5390d79029dd04f3968d593f75da33407b82f946
gxldnn <riberajan@gmail.com>
2025-10-03 19:42:06 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/20.compactar.sh
+++ Administracio de Sistemes/Ac1.2/20.compactar.sh
@@ -74,7 +74,7 @@ main(){
             fi
             read -p "El vols comprimir? (s/n)" compactarnew # demana si el vol compactar
             if [ "$compactarnew" = "s" ]; then
-                read -p "A on el vols compactar? no escriguis res per a guardar en el directori actual: " pathnew
+                read -p "A on el vols compactar? no escriguis res per a guardar en el directori actual: " pathnew # demana on
                 if [ -z "$pathnew" ]; then
                     pathnew="$(pwd)"
                 fi
