3cd72e7cdcf78f5952ee1bc46db0d6c5aa5e39d2
gxldnn <riberajan@gmail.com>
2025-10-03 18:38:33 +0000

    ClassPush

--- Administracio de Sistemes/Ac1.2/08.verifica.numprim.sh
+++ Administracio de Sistemes/Ac1.2/08.verifica.numprim.sh
@@ -4,11 +4,11 @@ read -p "Indica un numero per a comprovar si es prim o no: " num
 count=0
 for((i=2; i<=$num; i++)); do # For que va des del 2 fins al numero introduit
     if [ $(($num  % $i)) -eq 0 ]; then # Si la divisio  es exaxcta "-eq 0" +
-        (( count++ )) # Sumem 1 al comptadot
+        (( count++ )) # Sumem 1 al comptador
     fi
 done
 
-if [ $count = 1 ]; then
+if [ $count = 1 ]; then # Si el comptador es 1 es prim
     echo "El numero es prim"
 else
     echo "El numero no es prim"
